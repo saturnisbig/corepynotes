fa726d1f2a992f070205bcce2d8775bbb2dc70e7
saturnisbig <i.kenting@gmail.com>
2019-03-17 15:06:47 +0800

    Add client tcp script.

--- tsTclnt.py
+++ tsTclnt.py
@@ -4,9 +4,22 @@
 from socket import *
 
 HOST = 'localhost'
+REMOTE_HOST = '66.112.209.81'
 PORT = 21567
 BUFSIZE = 1024
-ADDR = (HOST, PORT)
+ADDR = (REMOTE_HOST, PORT)
 
 tcpCliSock = socket(AF_INET, SOCK_STREAM)
 tcpCliSock.connect(ADDR)
+
+while True:
+    data = raw_input('> ')
+    if not data:
+        break
+    tcpCliSock.send(data)
+    data = tcpCliSock.recv(BUFSIZE)
+    if not data:
+        break
+    print data
+
+tcpCliSock.close()
